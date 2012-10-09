a087e579a0349845cba81d930e80e12314c6be66
Tate Tian <tatetian@gmail.com>
2012-10-09 15:22:44 +0800

    First commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+gconftool-2 -s -t string /apps/guake/style/background/color "#101010101010"
+gconftool-2 -s -t string /apps/guake/style/font/color "#f2f2f2f2f2f2"
+gconftool-2 -s -t string /apps/guake/style/font/palette "#303030303030:#e1e132321a1a:#6a6ab0b01717:#ffffc0c00505:#00004f4f9e9e:#ecec00004848:#2a2aa7a7e7e7:#f2f2f2f2f2f2:#5d5d5d5d5d5d:#ffff36361e1e:#7b7bc9c91f1f:#ffffd0d00a0a:#00007171ffff:#ffff1d1d6262:#4b4bb8b8fdfd:#a0a02020f0f0"
